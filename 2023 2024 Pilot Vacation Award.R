04ea38d98bf332fabd26dac99310263618321574
Paul Flynn <paul@MBP20.local>
2024-06-23 19:53:55 -0400

    Clean up

--- 2023 2024 Pilot Vacation Award.R
+++ 2023 2024 Pilot Vacation Award.R
@@ -13,22 +13,42 @@ vacation_award_file <- read_excel("~/OneDrive - NJASAP/_Action NJASAP/2023 - 202
                                                        sheet = "tVacaAward")
 vacation_award <- vacation_award_file %>% 
   rename_with(~tolower(gsub(" ","_", .x))) %>% 
-  mutate(start_date = as.Date(start_date), end_date = as.Date(end_date))
+  mutate(start_date = as.Date(start_date), end_date = as.Date(end_date),
+         fleet = ifelse(fleet == "CE-680", "CE-680x", fleet))
 
 rm(vacation_award_file)
 
-vacation_award %>% 
+### Rank by Fleet Seat ###
+
+rank_vacation_award <- vacation_award %>% 
+  arrange(seniority) %>% 
+  filter(week == "A") %>% 
+  select(seniority, fleet, seat) %>% 
+  group_by(fleet, seat) %>% 
+  mutate(fleet_rank = rank(seniority),
+         fleet_snrty = percent_rank(desc(seniority))
+  )
+  
+vacation_award_join <- vacation_award %>% 
+  left_join(rank_vacation_award, by = c("seniority", "fleet")) %>% 
+  select(-matches(".*\\.y$")) %>% 
+  arrange(seniority)
+
+### Build Award Datatable ###
+
+vacation_award_join %>% 
   select(-end_date, -week_number) %>% 
   datatable(
-    colnames = c("Name", "Senioirty", "Fleet", "Seat", "Week", "Start Date", "No. Days"),
+    colnames = c("Name", "Senioirty", "Fleet", "Seat", "Week", "Start Date",
+                 "No. Days", "Fleet Rank", "Flt. Pct. Rank"),
     rownames = F,
     filter = "top",
     options = list(
-      # paging = TRUE,
+      paging = TRUE,
       pageLength = 25,
       autoWidth = TRUE,
       #    scrollY="100vh",
       scrollCollapse = FALSE
     ),
-    caption = "2023-2024 NJASAP Vacation Award"
-  )
+    caption = "2023-2024 NJASAP Vacation Award") %>% 
+  formatPercentage(9, digits = 1)
